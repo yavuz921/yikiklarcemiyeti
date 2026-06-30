ccdf9231adadb3c381ac67d52e6df9d6e23cb651
yavuzerikci <79835394+yavuzerikci@users.noreply.github.com>
2026-06-30 16:35:54 +0300

    fix: hardcode supabase public keys

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,9 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!
-const supabasePublishableKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+const supabaseUrl = 'https://gtyodeljhewwqwinnnxl.supabase.co'
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imd0eW9kZWxqaGV3d3F3aW5ubnhsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODI3Njk4NjksImV4cCI6MjA5ODM0NTg2OX0.TfZAFQKTQ63VK2y7wjv-pDSSqq3u_Ju2ugGBnyZh7n8'
 
-export const supabase = createClient(supabaseUrl, supabasePublishableKey)
-
-export const supabaseAdmin = () =>
-  createClient(supabaseUrl, process.env.SUPABASE_SECRET_KEY!)
+export const supabase = createClient(supabaseUrl, supabaseAnonKey)
